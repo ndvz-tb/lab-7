52f2a42ff653cf7281e455f073e5b2263999213b
ndvz-tb <nedovizt26@gmail.com>
2025-12-12 13:00:02 +0200

    Update lab71.py

--- lab71.py
+++ lab71.py
@@ -39,25 +39,25 @@ A = [
     [1, 74, -2, 80, -1],
     [10, 30, 20, -50, 91],
     [2, 4, 5, 81, 0],
-    [33, 69, -5, 51, 24]
+    [33, 69, -5, 51, 24],
 ]
 
-print("Початкова матриця:")
-for row in A:
-    print(row)
+if __name__ == "__main__":
+    print("Початкова матриця:")
+    for row in matrix:
+        print(row)
 
-bubble_sort_columns(A)
+    bubble_sort_columns(matrix)
 
-print("Матриця після сортування стовпців:")
-for row in A:
-    print(row)
+    print("Матриця після сортування стовпців:")
+    for row in matrix:
+        print(row)
 
-f_values = [geometric_mean(row) for row in A]
+    f_values = [geometric_mean(row) for row in matrix]
 
-print("Середні геометричні значення рядків:")
-for i, value in enumerate(f_values, start=1):
-    print(f"f({i}) = {value}")
+    print("Середні геометричні значення рядків:")
+    for i, value in enumerate(f_values, start=1):
+        print(f"f({i}) = {value}")
 
-F_value = arithmetic_mean(f_values)
-
-print("Середнє арифметичне значень f(i):", F_value)
+    F_value = arithmetic_mean(f_values)
+    print("Середнє арифметичне значень f(i):", F_value)
